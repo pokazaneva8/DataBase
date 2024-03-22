0ebf896c4464397c2a7b5ca84d0b4c89bd6cbd2e
Polina <79976433+pokazaneva8@users.noreply.github.com>
2024-03-23 00:00:49 +0500

    Create createSequence.sql
    Скрипт создания последовательностей для таблиц: Клиент, Сотрудник, Услуги, Должность, Запись.

---
+++ createSequence.sql
@@ -0,0 +1,34 @@
+create sequence seq_cli 
+increment by 1 
+start with 1 
+minvalue 1 
+maxvalue 9999 
+cycle;
+
+create sequence seq_emp 
+increment by 1 
+start with 1 
+minvalue 1 
+maxvalue 9999 
+cycle;
+
+create sequence seq_serv 
+increment by 1 
+start with 1 
+minvalue 1 
+maxvalue 9999 
+cycle;
+
+create sequence seq_post 
+increment by 1 
+start with 1 
+minvalue 1 
+maxvalue 9999 
+cycle;
+
+create sequence seq_re 
+increment by 1 
+start with 1 
+minvalue 1 
+maxvalue 9999 
+cycle;
